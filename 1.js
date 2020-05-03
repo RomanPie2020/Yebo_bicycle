7dff36f0267e67c4f938912bc7f55e16b5b057dd
RomanPie2020 <romanstone2004@gmail.com>
2020-05-03 10:32:13 +0300

    almost ready

--- 1.js
+++ 1.js
@@ -1,18 +1,23 @@
-//  slider
 $(document).ready(function () {
+    //  slider
     $('.slider').slick({
         arrows: false,
         autoplay: true,
         dots: true,
     });
-});
 
-// burger menu
-$(document).ready(function () {
+
+    // burger menu
     $('.burger-btn').click(function (event) {
         $('.burger-btn, .burger-menu').toggleClass('active');
         $('body').toggleClass('lock');
         
     });
+    $('.burger-menu-link').click(function (event) {
+        $('.burger-btn, .burger-menu').removeClass('active');
+        $('body').removeClass('lock');
+    });
     
+    //
+    $('.slick-dots').attr('id', 'slick-dots');
 });
